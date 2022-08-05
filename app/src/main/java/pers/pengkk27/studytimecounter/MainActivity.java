9d68e8dd4f9fc27a2eb2db013b6d9603380da98e
pengkk27 <1481128018@qq.com>
2022-08-05 23:09:05 +0800

    layout文件中元素添加id

--- app/src/main/java/pers/pengkk27/studytimecounter/MainActivity.java
+++ app/src/main/java/pers/pengkk27/studytimecounter/MainActivity.java
@@ -3,8 +3,14 @@ package pers.pengkk27.studytimecounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button getUp;
+    private Button sleep;
+    private Button learning;
 
 
 
@@ -12,5 +18,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getUp = findViewById(R.id.getup);
+        sleep = findViewById(R.id.sleep);
+        learning = findViewById(R.id.learning);
+
+
+
     }
 }
