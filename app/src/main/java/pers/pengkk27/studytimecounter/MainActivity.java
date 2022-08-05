9461fd05e2221d2b7a93552d526388b63afe2ee0
pengkk27 <1481128018@qq.com>
2022-08-05 22:11:32 +0800

    第ä¸创建版æé项目，ä½辨æ编写第一部版本

---
+++ app/src/main/java/pers/pengkk27/studytimecounter/MainActivity.java
@@ -0,0 +1,14 @@
+package pers.pengkk27.studytimecounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
