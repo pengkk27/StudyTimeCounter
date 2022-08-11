e507382ee576632c58c9fbf51414335425bb4670
pengkk27 <1481128018@qq.com>
2022-08-11 22:34:52 +0800

    编写判断时间格式是否正确的方法

--- app/src/main/java/pers/pengkk27/studytimecounter/utils/TimeUtil.java
+++ app/src/main/java/pers/pengkk27/studytimecounter/utils/TimeUtil.java
@@ -1,8 +1,11 @@
 package pers.pengkk27.studytimecounter.utils;
 
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @author pengkk27
- *
+ * <p>
  * 时间计算工具类
  */
 public class TimeUtil {
@@ -12,13 +15,38 @@ public class TimeUtil {
      * 判断输入的文字时间是否合理
      */
     public boolean adjustTime(String timeString) {
-
-
+        // 先判断是否为空值
+        if (StringUtils.isBlank(timeString)) {
+            return false;
+        }
 
         // 判断是否为一个时刻
-        boolean contains = timeString.contains(":");
+        if (!timeString.contains(":") || !timeString.contains("：")) {
+            return false;
+        }
 
-        return false;
+        // 拆分分号左右两边的数字
+        String[] timeList = timeString.split("[:|：]");
+        if (ObjectUtils.isEmpty(timeList) || timeList.length < 2) {
+            return false;
+        }
+
+        int hour;
+        int minute;
+        try {
+            hour = Integer.parseInt(timeList[0]);
+            minute = Integer.parseInt(timeList[1]);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        if (hour < 0 || hour > 24) {
+            return false;
+        }
+        if (minute < 0 || minute > 60) {
+            return false;
+        }
+
+        return true;
     }
 
 
